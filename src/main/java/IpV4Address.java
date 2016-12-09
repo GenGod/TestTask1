bb0f4e971c9685e1ab7b3b88b99efb2c3c470ad6
Богдан Ветренко <gengodoriginal@gmail.com>
2016-12-09 23:32:30 +0300

    Commit3
    Redisigned class IpV4Address.

--- src/main/java/IpV4Address.java
+++ src/main/java/IpV4Address.java
@@ -5,46 +5,18 @@ import java.util.Scanner;
  */
 public class IpV4Address {
 
-    private short firstPart;
-    private short secondPart;
-    private short thirdPart;
-    private short fourthPart;
+    private long add;
 
     public IpV4Address(String address) {
         String[] addresses = address.split("[.]");
-        firstPart = Short.parseShort(addresses[0]);
-        secondPart = Short.parseShort(addresses[1]);
-        thirdPart = Short.parseShort(addresses[2]);
-        fourthPart = Short.parseShort(addresses[3]);
-    }
-
-    public IpV4Address(short firstPart, short secondPart, short thirdPart, short fourthPart) {
-        this.firstPart = firstPart;
-        this.secondPart = secondPart;
-        this.thirdPart = thirdPart;
-        this.fourthPart = fourthPart;
+        add = Long.parseLong(addresses[0].concat(addresses[1].concat(addresses[2].concat(addresses[3]))));
     }
 
     public void printAll() {
-        System.out.println(this.firstPart);
-        System.out.println(this.secondPart);
-        System.out.println(this.thirdPart);
-        System.out.println(this.fourthPart);
+        System.out.println(this.add);
     }
 
-    public short getFirstPart() {
-        return firstPart;
-    }
-
-    public short getSecondPart() {
-        return secondPart;
-    }
-
-    public short getThirdPart() {
-        return thirdPart;
-    }
-
-    public short getFourthPart() {
-        return fourthPart;
+    public long getAdd() {
+        return add;
     }
 }
