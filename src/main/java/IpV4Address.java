99a1439176a7c402de75677d28610827c9ecf60f
Богдан Ветренко <gengodoriginal@gmail.com>
2016-12-10 00:01:36 +0300

    Commit 4
    Refactor IpV4Address class and added Main function.

--- src/main/java/IpV4Address.java
+++ src/main/java/IpV4Address.java
@@ -8,8 +8,57 @@ public class IpV4Address {
     private long add;
 
     public IpV4Address(String address) {
-        String[] addresses = address.split("[.]");
-        add = Long.parseLong(addresses[0].concat(addresses[1].concat(addresses[2].concat(addresses[3]))));
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (String ad : address.split("[.]")) {
+                switch (ad.length()) {
+                    case 1: {
+                        StringBuilder sb = new StringBuilder(ad);
+                        sb.insert(0, "00");
+                        stringBuilder.append(sb.toString());
+                        break;
+                    }
+                    case 2: {
+                        if (Short.parseShort(ad) > 0) {
+                            StringBuilder sb = new StringBuilder(ad);
+                            sb.insert(0, "0");
+                            stringBuilder.append(sb);
+                            break;
+                        }
+                        else {
+                            StringBuilder sb = new StringBuilder(ad);
+                            sb.deleteCharAt(0);
+                            sb.insert(0, "00");
+                            stringBuilder.append(sb.toString());
+                            break;
+                        }
+                    }
+                    case 3: {
+                        if (Short.parseShort(ad) > 0) {
+                            if (Short.parseShort(ad) < 256) {
+                                stringBuilder.append(ad);
+                            } else {
+                                System.out.println("Parts of ip must be less than 256! This part will equals \"255\".");
+                                stringBuilder.append("255");
+                            }
+                            break;
+                        } else {
+                            StringBuilder sb = new StringBuilder(ad);
+                            sb.deleteCharAt(0);
+                            sb.insert(0, "0");
+                            stringBuilder.append(sb);
+                            break;
+                        }
+                    }
+                    default: {
+                        stringBuilder.append("255");
+                    }
+                }
+            }
+            add = Long.parseLong(stringBuilder.toString());
+        } catch (NullPointerException e) {
+            add = 0;
+        }
     }
 
     public void printAll() {
---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * Created by Богдан on 09.12.2016.
+ */
+public class Main {
+
+    public static long substraction (IpV4Address minued, IpV4Address subtrahend) {
+        return Math.abs(minued.getAdd() - subtrahend.getAdd());
+    }
+
+    public static void main(String[] args) {
+        String address0;
+        String address1;
+        System.out.println("Welcome! Pleas, input olny digits and dot-symbols. Thank you!");
+        Scanner s = new Scanner(System.in);
+        do {
+            System.out.println("Input first ip:");
+            address0 = s.nextLine();
+            if (address0.length() < 7 || address0.length() > 15) {
+                System.out.println("Input another ip.");
+            }
+        }
+        while (address0.length() < 7 || address0.length() > 15);
+        do {
+            System.out.println("Input second ip:");
+            address1 = s.nextLine();
+            if (address1.length() < 7 || address0.length() > 15) {
+                System.out.println("Input another ip.");
+            }
+        }
+        while (address0.length() < 7 || address0.length() > 15);
+        IpV4Address add0 = new IpV4Address(address0);
+        add0.printAll();
+        IpV4Address add1 = new IpV4Address(address1);
+        add1.printAll();
+        System.out.println(substraction(add0, add1));
+    }
+}
