39885d0a84f9516413f6292cf17849a26489502d
Богдан Ветренко <gengodoriginal@gmail.com>
2016-12-09 23:14:15 +0300

    Commit 2
    Added getters and new constructor fot IpV4Address class.

--- src/main/java/IpV4Address.java
+++ src/main/java/IpV4Address.java
@@ -18,10 +18,33 @@ public class IpV4Address {
         fourthPart = Short.parseShort(addresses[3]);
     }
 
+    public IpV4Address(short firstPart, short secondPart, short thirdPart, short fourthPart) {
+        this.firstPart = firstPart;
+        this.secondPart = secondPart;
+        this.thirdPart = thirdPart;
+        this.fourthPart = fourthPart;
+    }
+
     public void printAll() {
         System.out.println(this.firstPart);
         System.out.println(this.secondPart);
         System.out.println(this.thirdPart);
         System.out.println(this.fourthPart);
+    }
+
+    public short getFirstPart() {
+        return firstPart;
+    }
+
+    public short getSecondPart() {
+        return secondPart;
+    }
+
+    public short getThirdPart() {
+        return thirdPart;
+    }
+
+    public short getFourthPart() {
+        return fourthPart;
     }
 }
